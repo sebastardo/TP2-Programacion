888f9396728669d797237960ea62b9567151800f
Sebastardo <sebastardo@outlook.com>
2020-05-10 02:12:53 -0300

    Acomodado main

--- main.c
+++ main.c
@@ -8,10 +8,10 @@ int main()
     do
     {
         opcion = menu_ejercicios("Elija un ejercicio para ejecutar (1 a 21 y 0 para finalizar): ", opciones);
+
         ejercicio(opcion);
+
     }while(opcion!='X');
-
-
 
     return 0;
 }
